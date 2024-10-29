8804ef314085229dbeaad92c8fedfa08044246f1
mty <benshiben@163.com>
2024-10-29 15:59:38 +0800

    TS支持

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module "@/utils/validate" {
+    export const Validate: {
+        IDCardCheck: (value: string) => boolean;
+        emailCheck: (value: string) => boolean;
+        mobileCheck: (value: string) => boolean;
+    };
+    export const Format: {
+        formatMoney: (money: number, symbol?: string, decimals?: number) => string;
+    };
+}
